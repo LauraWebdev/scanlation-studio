59138c82f08236c4bec9a49e8a07f4414b6459b8
Andreas Heimann <andreas@iklikla.de>
2019-04-18 11:08:26 +0200

    app.js now accepts the IPC Event for page transition

--- src/app/assets/js/app.ui.js
+++ src/app/assets/js/app.ui.js
@@ -1,7 +1,10 @@
+const electron = require('electron');
+const { dialog, remote } = electron.remote;
 const ProjectTools = require('./assets/js/project.functions.js');
 const RenderTools = require('./assets/js/renderer.functions.js');
 
 // REFERENCES
+let renderplane = document.querySelector(".renderplane");
 
 // UI FUNCTIONS
 function uiBackToSplash() {
@@ -9,7 +12,22 @@ function uiBackToSplash() {
 }
 
 // RUNTIME
-let project = ProjectTools.loadProject( "debug/" );
-let renderplane = document.querySelector(".renderplane");
+electron.ipcRenderer.on('transition-loadproject', (event, args) => {
+    let projectPath = args;
 
-RenderTools.render(renderplane, project, 1, 1);
+    console.log("[APP] Loading Project...");
+    console.log("[APP] ProjectPath: " + projectPath);
+    if(projectPath !== undefined || projectPath !== "") {
+        let project = ProjectTools.loadProject(projectPath);
+
+        if(project[0] === true) {
+            console.log("[APP] Rendering...");
+            RenderTools.render(renderplane, project[1], 1, 1);
+        } else {
+            console.error("[APP] Couldn't load Project!");
+            console.error(project[1]);
+        }
+    } else {
+        console.error("[APP] ProjectPath is not set!");
+    }
+});
