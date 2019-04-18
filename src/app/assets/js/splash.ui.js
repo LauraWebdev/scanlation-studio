226a81d867cc7851a4c019a62fea42bd713a1aa2
Andreas Heimann <andreas@iklikla.de>
2019-04-18 11:07:45 +0200

    uiNewProject now initiates newProject and propertly sends an IPC Event for page transition

--- src/app/assets/js/splash.ui.js
+++ src/app/assets/js/splash.ui.js
@@ -1,3 +1,5 @@
+const electron = require('electron');
+const { dialog, remote } = electron.remote;
 const ProjectTools = require('./assets/js/project.functions.js');
 
 // REFERENCES
@@ -11,10 +13,32 @@ function loadProjects() {
 // UI FUNCTIONS
 function uiNewProject() {
     // Show OPEN FOLDER Dialog
-    // TODO
+    let newProjectPath = dialog.showOpenDialog({
+        properties: ['openDirectory']
+    });
 
-    // Create new Project
-    ProjectTools.newProject("debug1234");
+    if(newProjectPath !== undefined) {
+        let newProjectSlug = "debug-" + Date.now().toString();
+
+        console.log("[UI][NewProject] ProjectPath: " + newProjectPath);
+        console.log("[UI][NewProject] ProjectSlug: " + newProjectSlug);
+
+        // Create new Project
+        let projectCreationResult = ProjectTools.newProject(newProjectPath, newProjectSlug);
+        if(projectCreationResult[0] === true) {
+            // Add Project to latest Projects
+            // TODO
+
+            // Project created successfully, open project!
+            electron.ipcRenderer.send('transition-loadproject', newProjectPath + "/" + newProjectSlug);
+        } else {
+            // Project could not be created, error display!
+            console.error("[UI][NewProject] Project Creation wasn't successful!");
+            console.error(projectCreationResult[1]);
+        }
+    } else {
+        console.error("[UI][NewProject] Cancelled Path Selection");
+    }
 }
 function uiOpenProject( projectPath ) {
     // Show OPEN FOLDER Dialog
