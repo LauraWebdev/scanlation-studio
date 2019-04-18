fb773ad0e4792a15fe7978900cb0749b91792480
Andreas Heimann <andreas@iklikla.de>
2019-04-18 11:06:48 +0200

    newProject now creates project folders, dummy ProjectSettings. loadProject returns debug project

--- src/app/assets/js/project.functions.js
+++ src/app/assets/js/project.functions.js
@@ -1,31 +1,42 @@
+const fs = require('fs');
+const path = require('path');
 const Project = require('./models/Project.js');
 const ProjectSettings = require('./models/ProjectSettings.js');
 
 function loadProject( projectPath ) {
-    console.log("[PROJECT] Load Project: " + projectPath);
+    console.log("[PROJECT][LOAD] Project Path: " + projectPath);
 
     // Load Settings JSON
-    return new Project();
+    let project = new Project();
+
+    return [true, project];
 }
 
-function newProject( projectPath, projectTitle, projectOriginalTitle ) {
-    console.log("{PROJECT] Create Project: " + projectPath);
+function newProject( projectPath, projectSlug, projectTitle, projectOriginalTitle ) {
+    // Check if folder already exists
+    if(fs.existsSync(projectPath + "/" + projectSlug)) {
+        return [false, "Folder already exists!"];
+    }
 
     // Create Folders
-    // TODO
+    try {
+        fs.mkdirSync(projectPath + "/" + projectSlug);
+        fs.mkdirSync(projectPath + "/" + projectSlug + "/Chapters");
 
-    // Create Settings JSON
-    let newProjectSettings = new ProjectSettings(projectTitle, projectOriginalTitle);
-    let newProject = new Project([], newProjectSettings);
+        // Create Settings JSON
+        let newProjectSettings = new ProjectSettings(projectTitle, projectOriginalTitle);
+        let newProject = new Project([], newProjectSettings);
 
-    // Save new Project
-    let fileData = JSON.stringify(newProject);
-    // TODO
+        // Save new Project
+        let fileData = JSON.stringify(newProject);
+        fs.writeFileSync(projectPath + "/" + projectSlug + "/ProjectSettings.sls", fileData);
 
-    console.log(fileData);
-
-    // Load Project
-    this.loadProject( projectPath);
+        return [true, "Project created successfully!"];
+    } catch(e) {
+        console.error("[PROJECT][CREATE] Couldn't create project!");
+        console.error(e.toString());
+        return [false, e.toString()];
+    }
 }
 
 module.exports = {
